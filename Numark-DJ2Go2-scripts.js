980e909e86631ad0aa23e6a073bebca585d0dddd
Alex <shaftoe@users.noreply.github.com>
2020-06-13 12:56:48 +0300

    Add recommended init/shutdown functions to JS file

--- Numark-DJ2Go2-scripts.js
+++ Numark-DJ2Go2-scripts.js
@@ -4,17 +4,22 @@
  * Original mapping files downloaded from: https://www.mixxx.org/forums/viewtopic.php?f=7&t=13376
  */
 
-var wheelTurnFnc = function (chNumber) {
+var buildWheelTurn = function (chNumber) {
     return function (channel, control, value, status, group) {
         var newValue = (value < 64) ? value : value - 128;
         engine.setValue('[Channel' + chNumber + ']', 'jog', newValue); // Pitch bend
     }
-}
+};
 
 var NumarkDJ2Go2 = {
-    wheelTurn1: wheelTurnFnc(1),
-    wheelTurn2: wheelTurnFnc(2),
-    deckRate: function(midichan, control, value, status, group) {
+    wheelTurn1: buildWheelTurn(1),
+    wheelTurn2: buildWheelTurn(2),
+
+    deckRate: function (channel, control, value, status, group) {
         engine.setValue(group, 'rate', script.midiPitch(0, value, 0xE0));
     },
+
+    init: function (id, debugging) {},
+
+    shutdown: function () {},
 };
